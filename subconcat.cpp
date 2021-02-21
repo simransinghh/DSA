0ce47e5bba4cef7a69d07bc3b571888ebb17d0d1
simransinghh <65584589+simransinghh@users.noreply.github.com>
2021-02-22 01:45:29 +0530

    Create subconcat.cpp

---
+++ subconcat.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<int> findSubstring(string s, vector<string>& words) {
+        vector<int> ans;
+        int n = words[0].length();
+        int len = s.length(); 
+        int m = words.size();
+        int concat = n*m;
+        if(len<concat){
+            return ans;
+        }
+        unordered_map<string,int> word;
+
+        for(int i=0 ; i < m ; i++)
+        {
+            word[words[i]]++;
+        }
+        for(int i = 0 ; i < len-concat+1 ; i++){
+            unordered_map<string,int> check;
+            int j=i;
+            for(j;j<i+concat;j+n){
+                string sub = s.substr(j,n);
+                if(word.find(sub)==word.end()){
+                    break;
+                }else{
+                    check[sub]++;
+                    if(check[sub]>word[sub]){
+                        break;
+                    }
+                }
+            }
+            if(j==i+concat){
+                ans.push_back(i);
+            }       
+        }
+        
+        return ans;
+                                                                  
+    }
+};
