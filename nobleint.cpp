e4a086ca1b02ee6d3baa0a4182c2a55fa9f7fa84
simransinghh <65584589+simransinghh@users.noreply.github.com>
2021-05-09 20:45:03 +0530

    Update nobleint.cpp

--- nobleint.cpp
+++ nobleint.cpp
@@ -3,7 +3,10 @@ int Solution::solve(vector<int> &A) {
     int n= A.size();
     sort(A.begin(), A.end());
     for(int i=0;i<n;i++){
-        if(A[i]==n-1-i){
+        if(A[i]==A[i+1] && i != n-1){
+            continue;
+        }
+        else if(A[i]==n-1-i){
             return 1;
         }
     }
